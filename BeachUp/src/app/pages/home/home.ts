466ef07deadccdf09c50992fa9d9797e3a77a3a0
RenanWARZ <renanpilan711@gmail.com>
2026-03-12 23:38:13 -0300

    Tela_inicial_Renan

---
+++ BeachUp/src/app/pages/home/home.ts
@@ -0,0 +1,22 @@
+/* Typescript com rotas e funções do projeto */
+
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatAnchor } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { NavigationService } from '../../shared/services/navigation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatCardModule, MatAnchor, MatIcon],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  constructor(public navigation: NavigationService) {}
+
+  irPara(rota: string) {
+    this.navigation.irPara(rota);
+  }
+}
