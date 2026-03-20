e5b44d82cf8a0d2c795102675258ffcbbcd3ad0f
RenanWARZ <renanpilan711@gmail.com>
2026-03-19 22:28:47 -0300

    05 - Video, funções e estilização

--- FrontEnd/src/app/pages/home/home.ts
+++ FrontEnd/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { NavigationService } from '../../shared/services/navigation';
@@ -12,11 +12,27 @@ import { RouterLink } from '@angular/router';
   styleUrl: './home.css',
 })
 
-export class Home {
+export class Home implements AfterViewInit{
   constructor(public navigation: NavigationService) {}
 
-  /*Definir a rota IrPara */
   irPara(rota: string) {
     this.navigation.irPara(rota);
+  }
+
+  @ViewChild('sobreVideo') sobreVideo!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const video = this.sobreVideo.nativeElement;
+
+    video.addEventListener('mouseenter', () => {
+      video.play().catch((err) => {
+        console.log('Play não iniciado automaticamente:', err);
+      });
+    });
+
+    video.addEventListener('mouseleave', () => {
+      video.pause();
+      video.currentTime = 0;
+    });
   }
 }
