7955109ded3ee5b1da05a75a7f78f669ae96db68
Matheussokolovski <matheussokolovski@gmail.com>
2026-03-29 00:08:07 -0300

    Adicionando create e view da tela de pagamento

--- FrontEnd/src/app/pages/pagamento/pagamento.spec.ts
+++ FrontEnd/src/app/pages/pagamento/pagamento.spec.ts
@@ -1,20 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Pagamento } from './pagamento';
+import { NavigationService } from '../../shared/services/navigation';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 describe('Pagamento', () => {
   let component: Pagamento;
   let fixture: ComponentFixture<Pagamento>;
 
+var jasmine: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Pagamento]
-    })
-    .compileComponents();
+      imports: [Pagamento, ReactiveFormsModule], // 👈 AQUI
+      providers: [
+        {
+          provide: NavigationService,
+          useValue: {
+            irPara: jasmine.createSpy('irPara'),
+            getDados: jasmine.createSpy('getDados').and.returnValue({}),
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Pagamento);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges(); // 👈 AQUI
   });
 
   it('should create', () => {
--- FrontEnd/src/app/pages/pagamento/pagamento.ts
+++ FrontEnd/src/app/pages/pagamento/pagamento.ts
@@ -1,27 +1,59 @@
 import { Component } from '@angular/core';
 import { NavigationService } from '../../shared/services/navigation';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pagamento',
-  imports: [CommonModule],
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './pagamento.html',
   styleUrls: ['./pagamento.css'],
 })
-
 export class Pagamento {
   metodoSelecionado: 'cartao' | 'pix' = 'cartao';
+  formPagamento!: FormGroup;
+  mensagem: string = '';
 
-  // copiarPix(valor: string) {
-  //   navigator.clipboard.writeText(valor);
-  //   alert('Chave PIX copiada!');
-  // }
+  constructor(
+    public navigation: NavigationService,
+    private fb: FormBuilder,
+  ) {}
 
-  constructor(public navigation: NavigationService) {}
+  ngOnInit() {
+    this.formPagamento = this.fb.group({
+      nome: ['', Validators.required],
+      numeroCartao: ['', Validators.required],
+      validade: ['', Validators.required],
+      cvv: ['', Validators.required],
+      cpf: ['', Validators.required],
+    });
+  }
 
+  finalizarPagamento() {
+    console.log('clicou');
+
+    if (this.metodoSelecionado === 'cartao' && this.formPagamento.invalid) {
+      this.mensagem = '❌ Preencha os campos!';
+      return;
+    }
+
+    const dados = this.formPagamento.value;
+
+    console.log('Dados do formulário:', dados);
+    console.log('Nome:', dados.nome);
+    console.log('Cartão:', dados.numeroCartao);
+    console.log('Validade:', dados.validade);
+    console.log('Cpf:', dados.cpf);
+
+    this.mensagem = '⏳ Processando...';
+
+    setTimeout(() => {
+      console.log('indo para sucesso');
+      this.navigation.irPara('pagamento-sucesso');
+    }, 1000);
+  }
   irPara(rota: string) {
     this.navigation.irPara(rota);
   }
-  
 }
