04d3c8de706b64f3b5e467c2fff7e96b1d2f595a
Renan Ido <iddo.renan@gmail.com>
2026-03-30 02:57:25 -0300

    Criando Create e view para tela de avaliação

--- FrontEnd/src/app/pages/avaliacoes/avaliacoes.ts
+++ FrontEnd/src/app/pages/avaliacoes/avaliacoes.ts
@@ -1,18 +1,61 @@
 import { Component } from '@angular/core';
 import { NavigationService } from '../../shared/services/navigation';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-avaliacoes',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './avaliacoes.html',
   styleUrls: ['./avaliacoes.css']
 })
 
 export class Avaliacoes {
 
+  avaliacao: Avaliacao = {
+    nome: '',
+    comentario: '',
+    nota: 0
+  };
+
+  avaliacoes: Avaliacao[] = [];
+
+
   constructor(public navigation: NavigationService) {}
+
+
+  salvar() {
+    // validação
+    if (!this.avaliacao.nome || !this.avaliacao.comentario) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    if (this.avaliacao.nota < 0 || this.avaliacao.nota > 5) {
+      alert('A nota deve ser entre 0 e 5');
+      return;
+    }
+
+    // adiciona na lista
+    this.avaliacoes.push({ ...this.avaliacao });
+
+    // limpa formulário
+    this.avaliacao = {
+      nome: '',
+      comentario: '',
+      nota: 0
+    };
+  }
 
   irPara(rota: string) {
     this.navigation.irPara(rota);
   }
+}
+
+//MODEL
+interface Avaliacao {
+  nome: String;
+  nota: number;
+  comentario: String;
 }
