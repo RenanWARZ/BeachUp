023949f545609576637014fa351c9187b4132b3c
Matheussokolovski <matheussokolovski@gmail.com>
2026-04-07 00:33:06 -0300

    criando edit e delete na tela de pagamento

--- FrontEnd/src/app/pages/pagamento/pagamento.spec.ts
+++ FrontEnd/src/app/pages/pagamento/pagamento.spec.ts
@@ -1,32 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Pagamento } from './pagamento';
-import { NavigationService } from '../../shared/services/navigation';
-import { ReactiveFormsModule } from '@angular/forms';
 
+import { Pagamento } from './pagamento';
 
 describe('Pagamento', () => {
   let component: Pagamento;
   let fixture: ComponentFixture<Pagamento>;
 
-var jasmine: any;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Pagamento, ReactiveFormsModule], // 👈 AQUI
-      providers: [
-        {
-          provide: NavigationService,
-          useValue: {
-            irPara: jasmine.createSpy('irPara'),
-            getDados: jasmine.createSpy('getDados').and.returnValue({}),
-          },
-        },
-      ],
-    }).compileComponents();
+      imports: [Pagamento]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(Pagamento);
     component = fixture.componentInstance;
-    fixture.detectChanges(); // 👈 AQUI
+    await fixture.whenStable();
   });
 
   it('should create', () => {
